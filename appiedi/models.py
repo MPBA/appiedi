d22580824fb5096ec431326a42f7becec7b55bdc
franchg <Applet.147>
2014-01-21 17:43:24 +0100

    settings changed

--- appiedi/models.py
+++ appiedi/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class TelecomDataset(models.Model):
+    humidity = models.FloatField()
+    pressure = models.FloatField()
+    temperature = models.FloatField()
+    co = models.FloatField()
+    altitude = models.FloatField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    mtl_timestamp = models.DateTimeField()
+    the_geom = models.TextField(blank=True) # This field type is a guess.
+    accuracy = models.FloatField()
+    provider = models.IntegerField()
+    speed = models.FloatField()
+    id = models.BigIntegerField(primary_key=True)
+    class Meta:
+        managed = False
+        db_table = 'telecom_dataset'
--- appiedi/views.py
+++ appiedi/views.py
@@ -1,11 +1,32 @@
 from django.core.files.uploadedfile import UploadedFile
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.shortcuts import render
+from tojson import render_to_json
+from models import TelecomDataset
+from pyhive.extra.django import DjangoModelSerializer
+from pyhive.serializers import ListSerializer
+from pyhive.modifiers import exclude_fields
+import requests
+import json
 
 
 # Create your views here.
+@render_to_json(mimetype='application/json')
 def hello(request):
-    return HttpResponse('Hello Mondo', mimetype='text/plain')
+    td = TelecomDataset.objects.filter(pressure__gte=1)
+
+    #ser = ListSerializer(item_serializer=DjangoModelSerializer(
+        #[exclude_fields(['is_staff', 'is_superuser', 'password'])]
+    #))
+    #jsondata = ser.serialize(td)
+
+    url = 'http://collector-svil.mobileterritoriallab.eu/sensordrone/api/'
+    payload = {'timestamp_start': '21/01/2014 00:00', 'timestamp_end': '22/01/2014 00:00'}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+    #a = {'asd': 'asd', 'rec': 1, 'timestamp': '2014-01-21 11:11:11', 'data': jsondata, 'telecom': r.json()}
+    return r.json()
 
 
 # def process_list(request):
